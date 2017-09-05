8dad5c6c979ba799bee4b6b78a37f9f994f62683
Janaki <Janaki.marine@gmail.com>
2017-09-05 09:47:10 -0400

    Pixel Art Maker code changes
    These files contain the changes needed for pixel art maker project

--- designs.js
+++ designs.js
@@ -3,8 +3,47 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$(document).ready(function() {
+	$("#input_submit").click(function() {
+	var x = document.getElementById("input_height").value;
+	var y = document.getElementById("input_width").value;
+	var col = document.getElementById("colorPicker").value;
+	clearGrid();
+	makeGrid(x, y, col);
+});
+
+});
+
+
+/**
+*	@description Creates a grid of size x by y, adds event listeners dynamically
+	@param {number x} - row size
+	@param {number y} - column size
+	@param {color} - color to fill the cell on event trigger(mouseover)
+*
+*/
+function makeGrid(x, y, color) {
 
 // Your code goes here!
+	for (i=0; i < x; i++)
+	{
+		var row = document.getElementById("pixel_canvas").insertRow();
+		for (j=0; j < y; j++)
+		{
+			var cell = row.insertCell();
+			cell.innerHTML = " hi";
+			cell.addEventListener("mouseover", fillColor);
+			cell.myParam = color;
+		}
+	};
+};
 
-}
+function clearGrid() {
+	$("#pixel_canvas tr").remove();
+};
+
+function fillColor(evt) {
+	this.style.backgroundColor = evt.target.myParam;
+};
+
+
