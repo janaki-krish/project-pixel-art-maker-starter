27e9fad71eaf9688f590e2e568252b393cf0d3e1
Janaki <Janaki.marine@gmail.com>
2017-09-06 15:33:51 -0400

    Made changes as per the review comments
    Incorporated review comments

--- designs.js
+++ designs.js
@@ -3,8 +3,10 @@
 
 // When size is submitted by the user, call makeGrid()
 
+document.getElementById("sizePicker").addEventListener("submit", processSubmit, false);
 
-function processSubmit() {
+function processSubmit(event) {
+	event.preventDefault();
 	var x = document.getElementById("input_height").value;
 	var y = document.getElementById("input_width").value;
 	var col = document.getElementById("colorPicker").value;
@@ -12,47 +14,23 @@ function processSubmit() {
 	makeGrid(x, y, col);
 };
 
-/*
-$(document).ready(function() {
-	$("#input_submit").click(function() {
-	var x = document.getElementById("input_height").value;
-	var y = document.getElementById("input_width").value;
-	var col = document.getElementById("colorPicker").value;
-	clearGrid();
-	makeGrid(x, y, col);
-});
-
-});
-*/
-
-
-/**
-*	@description Creates a grid of size x by y, adds event listeners dynamically
-	@param {number x} - row size
-	@param {number y} - column size
-	@param {color} - color to fill the cell on event trigger(mouseover)
-*
-*/
 function makeGrid(x, y, color) {
 
 // Your code goes here!
+	var canvas = document.getElementById("pixel_canvas");
 	for (i=0; i < x; i++)
 	{
-		var row = document.getElementById("pixel_canvas").insertRow();
+		var row = canvas.insertRow();
 		for (j=0; j < y; j++)
 		{
 			var cell = row.insertCell();
 			cell.innerHTML = " hi";
-			cell.addEventListener("mouseover", fillColor);
+			cell.addEventListener("click", fillColor);
 			cell.myParam = color;
 		}
 	};
 };
-/*
-function clearGrid() {
-	$("#pixel_canvas tr").remove();
-};
-*/
+
 
 function clearGrid() {
 	document.getElementById("pixel_canvas").innerHTML = "";
